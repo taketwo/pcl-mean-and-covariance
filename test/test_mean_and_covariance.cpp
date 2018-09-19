5f5e29570a892495525ef7477ad919f547e4026a
M@t <msy22@uclive.ac.nz>
2018-09-19 13:47:00 +1200

    Got a basic addition function working (I think)

--- test/test_mean_and_covariance.cpp
+++ test/test_mean_and_covariance.cpp
@@ -1,4 +1,14 @@
 #include <gtest/gtest.h>
+#include <mean_and_covariance.hpp>
+
+TEST(TestCaseName, TestCase)
+{
+  int a = 1;
+  int b = 2;
+  int c = pcl::experimental::add_two_numbers(a,b);
+  EXPECT_EQ(c, 3);
+}
+
 
 int main (int argc, char** argv)
 {
